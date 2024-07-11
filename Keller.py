9f57114a89e920e73322fce20b15d5af545c4ece
K-TECH Industries (Keller Hydle) <151480646+ktechhydle@users.noreply.github.com>
2024-07-11 11:40:10 -0600

    Update Keller.py

--- Keller.py
+++ Keller.py
@@ -1,13 +1,20 @@
-from hydle_family import keller
+from hydle_family import qualities
 
-def me(name, age, qualities):
+def me() -> list[qualities]:
   name = 'Keller Hydle'
   age = 15
-  qualities = {'funny': keller.funny(), 
-               'kind': keller.kind(), 
-               'smart': keller.smart()
-              }
+  favorite_things = ['Snowboarding and Wakeboarding', 
+                     'Trampoline', 
+                     'Hiking',
+                     'Winter Camping',
+                     'Coding', 
+                     'German Learning',
+                     'Duolingo',
+                    'Math']
+  qualitites.append(name, age, favorite_things)
+  
+  return qualitites
 
-if __name__ == '__keller__':
-  k = keller()
-  k.me()
+if __name__ == '__main__':
+  list = me()
+  list.append('Do something great!')
