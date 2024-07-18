423eb8b9cc2cd2e226e4c3720b59d3b22be3c145
MP Software (Keller Hydle) <151480646+ktechhydle@users.noreply.github.com>
2024-07-18 13:37:58 -0600

    Create Keller.cpp

---
+++ Keller.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using std::string, std::cout;
+
+int main(){
+  string qualities[] = {"Snowboarding", "Wakeboarding", "German Learning", "Programming", "Math"}; // TODO: add more qualities
+  cout << "My qualities: " << qualities;
+  
+  return qualities
+}
